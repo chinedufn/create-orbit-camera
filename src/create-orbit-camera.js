b943f1ffed2abbdcddf4ab0399eb18ee25c28399
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2017-01-05 21:45:33 -0500

    Fix camera

--- src/create-orbit-camera.js
+++ src/create-orbit-camera.js
@@ -1,5 +1,8 @@
-var makeLookAt = require('./matrix-math/make-look-at.js')
 var mat4Multiply = require('gl-mat4/multiply')
+var mat4Translate = require('gl-mat4/translate')
+var mat4Invert = require('gl-mat4/invert')
+
+var makeLookAt = require('./matrix-math/make-look-at.js')
 var makeXRotation = require('./matrix-math/make-x-rotation.js')
 var makeYRotation = require('./matrix-math/make-y-rotation.js')
 
@@ -20,9 +23,11 @@ function createOrbitCamera (opts) {
   var cameraMatrix = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]
 
   // Translate the matrix to our camera position
-  cameraMatrix[12] = opts.position[0]
-  cameraMatrix[13] = opts.position[1]
-  cameraMatrix[14] = opts.position[2]
+  mat4Translate(cameraMatrix, cameraMatrix, [
+    opts.position[0],
+    opts.position[1],
+    opts.position[2]
+  ])
 
   // Rotate about the X and Y axis
   mat4Multiply(cameraMatrix, makeXRotation(0 - opts.xRadians), cameraMatrix)
@@ -37,6 +42,7 @@ function createOrbitCamera (opts) {
 
   // Look at the target
   cameraMatrix = makeLookAt(cameraPosition, opts.target, opts.up)
+  mat4Invert(cameraMatrix, cameraMatrix)
 
   // Return our camera's position and view matrix after rotating and looking at the target
   return {
